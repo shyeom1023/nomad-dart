86a5a4df41316a12396aef58c8c9d227c4eec517
shyeom1023 <shyeom1023@gmail.com>
2024-09-09 00:04:09 +0900

    class named constructor 학습 완료

--- classNamedConstructor.dart
+++ classNamedConstructor.dart
@@ -14,13 +14,18 @@ void main() {
     age: 22,
   );
   player2.sayHello();
+
+  var player3 = Player.createBluePlayer(name: '석현3', age: 14);
+  player3.sayHello();
+
+  var player4 = Player.createRedPlayer('석현4', 14);
+  player4.sayHello();
 }
 
 class Player {
   String name;
-  int xp;
+  int xp, age;
   String team;
-  int age;
 
   Player({
     required this.name,
@@ -29,5 +34,21 @@ class Player {
     required this.age,
   });
 
-  String sayHello() => "$name아 안녕";
+  // : 콜론은 클래스를 초기화 하는 뜻
+  Player.createBluePlayer({
+    required String name,
+    required int age,
+  })  : this.age = age,
+        this.name = name,
+        this.team = 'blue',
+        this.xp = 0;
+
+  // positional parameter는 기본적으로 required
+  Player.createRedPlayer(String name, int age)
+      : this.age = age,
+        this.name = name,
+        this.team = 'blue',
+        this.xp = 0;
+
+  void sayHello() => print("$name아 안녕");
 }
